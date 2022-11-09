5df267a02e0ba361fe46dff1724b866b0df4edef
MDeLuise <66636702+MDeLuise@users.noreply.github.com>
2022-11-09 14:29:15 +0100

    Add wave header to login screen

--- components/LoginScreen.tsx
+++ components/LoginScreen.tsx
@@ -2,18 +2,17 @@ import React, { useState } from "react";
 import {
   StyleSheet,
   View,
-  Image,
   Text,
-  SafeAreaView,
   TextInput,
-  ImageBackground,
   TouchableOpacity,
   KeyboardAvoidingView,
   Platform,
+  Linking
 } from "react-native";
 import { doGet } from "../common/ServerRequests";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GlobalStyles } from "../common/GlobalStyles";
+import WavyHeader from "./WeavyHeader";
 
 export default function LoginScreen({ navigation }: any) {
   const [hostAddress, setHostAddress]: [string, Function] = useState("http://localhost:3000");
@@ -54,58 +53,74 @@ export default function LoginScreen({ navigation }: any) {
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={{ flex: 1 }}>
-      <ImageBackground
-        style={styles.backgroundImg}
-        resizeMode="repeat"
-        source={require("../assets/dots.png")}>
-        <SafeAreaView style={styles.container}>
-          <Image style={styles.img} source={require("../assets/logo.png")} />
-          <View style={styles.view}>
-            <Text style={styles.text}>Login to your Account</Text>
-            <TextInput
-              style={{ ...GlobalStyles.input, width: "100%", marginVertical: 0 }}
-              placeholder="Host Address"
-              placeholderTextColor={"grey"}
-              onChangeText={(newAddress) => setHostAddress(newAddress)} />
-            <TextInput
-              style={{ ...GlobalStyles.input, width: "100%", marginVertical: 0 }}
-              placeholder="Api Key"
-              placeholderTextColor={"grey"}
-              secureTextEntry={true}
-              onChangeText={(newKey) => setApiKey(newKey)} />
-            <TouchableOpacity
-              style={GlobalStyles.btn}
-              onPress={() => login(hostAddress, apiKey)}>
-              <Text style={GlobalStyles.btnText}>Login</Text>
-            </TouchableOpacity>
-          </View>
-        </SafeAreaView>
-      </ImageBackground>
+      style={styles.container}>
+      <WavyHeader customStyles={{ position: "absolute", top: 0, width: "100%" }} />
+      <View style={styles.headerContainer}>
+        {/* <Text style={styles.headerText}>Custom Header</Text> */}
+      </View>
+      <View style={styles.view}>
+        <View>
+          <Text style={styles.text1}>Login</Text>
+          <Text>Don't have an account?<Text> </Text>
+            <Text style={styles.text2}
+              onPress={() => Linking.openURL('https://github.com/MDeLuise/tracky-client#usage')}>
+              Follow the instructions
+            </Text>
+          </Text>
+        </View>
+        <View>
+          <TextInput
+            style={{ ...GlobalStyles.input, width: "100%" }}
+            placeholder="Host Address"
+            placeholderTextColor={"grey"}
+            onChangeText={(newAddress) => setHostAddress(newAddress)} />
+          <TextInput
+            style={{ ...GlobalStyles.input, width: "100%", marginTop: 20, }}
+            placeholder="Api Key"
+            placeholderTextColor={"grey"}
+            secureTextEntry={true}
+            onChangeText={(newKey) => setApiKey(newKey)} />
+        </View>
+        <TouchableOpacity
+          style={GlobalStyles.btn}
+          onPress={() => login(hostAddress, apiKey)}>
+          <Text style={GlobalStyles.btnText}>Login</Text>
+        </TouchableOpacity>
+      </View>
     </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  backgroundImg: {
-    flex: 1,
-  },
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#f2f2f2",
   },
-  img: {
-    width: 100,
-    height: 100,
-  },
-  text: {
+  text1: {
     fontWeight: "bold",
+    fontSize: 50,
+  },
+  text2: {
+    paddingTop: 10,
+    fontSize: 13,
+    textDecorationLine: "underline"
   },
   view: {
     flex: 0.5,
     justifyContent: "space-around",
     width: "70%",
   },
+  headerContainer: {
+    marginTop: 50,
+    marginHorizontal: 10
+  },
+  headerText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#333',
+    textAlign: 'center',
+    marginTop: 35
+  }
 });
